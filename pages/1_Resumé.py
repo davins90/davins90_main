307e7ca502668282b1253c0ff3db4e20c880841c
dani <daniele.davino@live.it>
2023-12-24 01:34:43 +0100

    test path

--- pages/1_Resumé.py
+++ pages/1_Resumé.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 import os
-print(os.getcwd())
+st.wrtie(os.getcwd())
 
 
 utils.add_logo("../../images/foto_dani_cartoon.png","Daniele D'Avino",'Machine Learning Engineer @ Sky',200,"black",False)
